60a771ad9cd55f5a007fb4af0f501db40191f4aa
Dmitriy Balaev <DmBalaev91@gmail.com>
2023-07-08 16:12:35 +0300

    Init commit

---
+++ src/test/java/dm/balaev/Bookbooking/BookBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package dm.balaev.Bookbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
