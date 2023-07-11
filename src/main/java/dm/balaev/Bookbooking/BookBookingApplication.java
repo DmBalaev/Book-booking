cccd4d6e5cd6871c099efea6652c45a4d3ca1a6a
Dmitriy Balaev <DmBalaev91@gmail.com>
2023-07-11 12:51:50 +0300

    - added the ability to cache using Redis

--- src/main/java/dm/balaev/Bookbooking/BookBookingApplication.java
+++ src/main/java/dm/balaev/Bookbooking/BookBookingApplication.java
@@ -2,8 +2,10 @@ package dm.balaev.Bookbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class BookBookingApplication {
 
 	public static void main(String[] args) {
