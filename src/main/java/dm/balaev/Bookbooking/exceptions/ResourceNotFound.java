3c4120a635fff9413fa714428ee21b6464e6a375
Dmitriy Balaev <DmBalaev91@gmail.com>
2023-08-01 21:34:11 +0300

    - Optimization, removing similar classes and adding one common to them

--- src/main/java/dm/balaev/Bookbooking/exceptions/ResourceNotFound.java
+++ src/main/java/dm/balaev/Bookbooking/exceptions/ResourceNotFound.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class BorrowingNotFoundException extends RuntimeException{
-    public BorrowingNotFoundException(String message) {
+public class ResourceNotFound extends RuntimeException{
+    public ResourceNotFound(String message) {
         super(message);
     }
 }
